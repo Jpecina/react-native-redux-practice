172441591b8d706454b8c714655aa812ebac6494
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-04-28 11:32:22 -0500

    added reducer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { HeaderComponent } from "./Components/Common";
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <HeaderComponent headerTitle="Tech Stack" />
+      </View>
     </Provider>
   );
 };
